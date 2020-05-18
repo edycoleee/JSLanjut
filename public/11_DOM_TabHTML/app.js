40cf0b5763076a01fd3b252ab99e795129e9f050
edycoleee <edycoleee@gmail.com>
2020-05-18 13:07:02 +0700

    LATIHAN

---
+++ public/11_DOM_TabHTML/app.js
@@ -0,0 +1,28 @@
+//select class="about" 
+//select target semua (3) button class="tab-btn" target id
+//select semua (3) article class ="conten"
+const about = document.querySelector(".about")
+const btns = document.querySelectorAll(".tab-btn")
+const articles = document.querySelectorAll(".content")
+console.log(about,btns,articles);
+
+//event listener about, parent => target id
+about.addEventListener("click", function(e){
+    console.log(e.target.dataset.id);
+    const id = e.target.dataset.id
+    if (id) {
+        // jika active from other id  
+        btns.forEach(function(btn){
+            btn.classList.remove("active")
+            e.target.classList.add("active")
+        })
+        //hide other article
+        articles.forEach(function(article){
+            article.classList.remove("active")
+        })
+        const element = document.getElementById(id)
+        console.log('id element : ',element);
+        element.classList.add("active")
+    }
+    console.log(btns);
+})
